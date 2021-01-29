0adfd0a90f3c2519cb5813f5686ee50da8b8aec5
Foll3n <alexis.deguibert@gmail.com>
2021-01-29 18:26:28 +0100

    add possibility to click on promise item in view  #13

--- app/src/main/java/fr/gof/promesse/Adapter/SearchAdapter.kt
+++ app/src/main/java/fr/gof/promesse/Adapter/SearchAdapter.kt
@@ -1,37 +1,26 @@
 package fr.gof.promesse.Adapter
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import fr.gof.promesse.R
 import fr.gof.promesse.model.Promise
-import org.w3c.dom.Text
-
 
 class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    var title : TextView
-    var date : TextView
-    var description : TextView
-
-    init{
-        super.itemView
-        title=itemView.findViewById(R.id.title)
-        date = itemView.findViewById(R.id.date)
-        description = itemView.findViewById(R.id.description)
-        Log.d("TAG____________okkkkkkkkkk","jinitialise !!!!!!")
-    }
+    var title : TextView = itemView.findViewById(R.id.title)
+    var date : TextView = itemView.findViewById(R.id.date)
+    var description : TextView = itemView.findViewById(R.id.description)
 }
 
 class SearchAdapter(var context: Context, var listePromesses :  List<Promise>) : RecyclerView.Adapter<SearchViewHolder>(){
-
+    lateinit var itemView : View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-
         val inflater = LayoutInflater.from(parent.context)
-        var itemView = inflater.inflate(R.layout.layoutsearchitems, parent, false)
+        itemView = inflater.inflate(R.layout.layoutsearchitems, parent, false)
         return SearchViewHolder(itemView)
     }
 
@@ -39,7 +28,9 @@ class SearchAdapter(var context: Context, var listePromesses :  List<Promise>) :
         holder.title.setText(listePromesses.get(position).title)
         holder.date.setText(listePromesses.get(position).dateTodo.toString())
         holder.description.setText(listePromesses.get(position).description)
-
+        itemView.setOnClickListener { v ->
+            Toast.makeText(context, listePromesses[position].toString()+" sélectionné !" , Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
