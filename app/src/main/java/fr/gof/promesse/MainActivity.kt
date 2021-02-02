d1fe23756c47781aa92b8efabb668916f8f68a4d
furimari <vivi.nefertari123@gmail.com>
2021-02-02 14:35:16 +0100

    Ajout d'une fenêtre de confirmation quand la promesse à supprimer a des sous-tâches. #5

--- app/src/main/java/fr/gof/promesse/MainActivity.kt
+++ app/src/main/java/fr/gof/promesse/MainActivity.kt
@@ -1,15 +1,18 @@
 package fr.gof.promesse
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.get
 import androidx.core.view.marginTop
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import fr.gof.promesse.database.PromiseDataBase
 import fr.gof.promesse.model.Promise
 import fr.gof.promesse.model.State
+import fr.gof.promesse.model.Subtask
 import fr.gof.promesse.model.User
 import java.util.*
 
@@ -26,10 +29,11 @@ class MainActivity : AppCompatActivity() {
         val promesse = Promise(-1, "Titre", 5, State.TODO, true, "Desc", true, Date(System.currentTimeMillis()), Date(System.currentTimeMillis()), null)
         val promesse2 = Promise(-1, "Titre2", 5, State.TODO, true, "Desc2", true, Date(System.currentTimeMillis()), Date(System.currentTimeMillis()), null)
         val promesse3 = Promise(-1, "Titre3", 5, State.TODO, false, "Desc3", true, Date(System.currentTimeMillis()), Date(System.currentTimeMillis()), null)
-
+        val promesse4 = Promise(-1, "Titre4", 5, State.TODO, false, "Descr4", true, Date(System.currentTimeMillis()), Date(System.currentTimeMillis()), mutableListOf(Subtask(0, "Sstache", false)))
         //defaultUser.addPromise(promesse, promiseDataBase)
         //defaultUser.addPromise(promesse2, promiseDataBase)
         //defaultUser.addPromise(promesse3, promiseDataBase)
+        //defaultUser.addPromise(promesse4, promiseDataBase)
         /*println(defaultUser.getAllPromise(promiseDataBase).toString())
         label.setText(defaultUser.getAllPromise(promiseDataBase).toString())*/
 
@@ -38,7 +42,8 @@ class MainActivity : AppCompatActivity() {
         val listPromesse = setPromesse.toMutableList()
         val listPromesseDescr = mutableListOf<String>()
         for(promise in listPromesse) {
-          var s = if(promise.priority) { "★ " } else { "" } + "${promise.title}\n${promise.description}"
+          var s = if(promise.priority) { "★ " } else { "" } + "${promise.title}\n${promise.description}" + if (promise.subtasks != null) { "\nSous-tâches !" } else { "" }
+
           listPromesseDescr.add(s)
         }
 
@@ -51,11 +56,34 @@ class MainActivity : AppCompatActivity() {
             del.visibility = View.VISIBLE
             del.translationY = listView.marginTop.toFloat() + (view.height/10) + (id*view.height)
             del.setOnClickListener(View.OnClickListener { view ->
-                promiseDataBase.deletePromise(listPromesse.get(id.toInt()))
-                listPromesse.removeAt(id.toInt())
-                listPromesseDescr.removeAt(id.toInt())
-                adapter.notifyDataSetChanged()
-                del.visibility = View.INVISIBLE
+                if (listPromesse[id.toInt()].subtasks != null) {
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setMessage("Attention, cette promesse possède des sous-tâches. " +
+                            "Êtes-vous sûr(e) de vouloir la supprimer ?")
+                            .setCancelable(true)
+                            .setPositiveButton("Oui", DialogInterface.OnClickListener {
+                                _, _ -> promiseDataBase.deletePromise(listPromesse.get(id.toInt()))
+                                listPromesse.removeAt(id.toInt())
+                                listPromesseDescr.removeAt(id.toInt())
+                                adapter.notifyDataSetChanged()
+                                del.visibility = View.INVISIBLE
+                            })
+                            .setNegativeButton("Non", DialogInterface.OnClickListener {
+                                dialog, _ -> dialog.cancel()
+                            })
+
+                    val alert = dialogBuilder.create()
+                    alert.setTitle("Suppression de promesse")
+                    alert.show()
+
+                }
+                else {
+                    promiseDataBase.deletePromise(listPromesse.get(id.toInt()))
+                    listPromesse.removeAt(id.toInt())
+                    listPromesseDescr.removeAt(id.toInt())
+                    adapter.notifyDataSetChanged()
+                    del.visibility = View.INVISIBLE
+                }
             })
 
             true}
--- app/src/main/java/fr/gof/promesse/database/PromiseDataBase.kt
+++ app/src/main/java/fr/gof/promesse/database/PromiseDataBase.kt
@@ -5,10 +5,7 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteConstraintException
 import android.database.sqlite.SQLiteDatabase
-import fr.gof.promesse.model.Mascot
-import fr.gof.promesse.model.Promise
-import fr.gof.promesse.model.State
-import fr.gof.promesse.model.User
+import fr.gof.promesse.model.*
 import java.lang.Exception
 import java.lang.IllegalArgumentException
 import java.text.SimpleDateFormat
@@ -30,6 +27,13 @@ class PromiseDataBase (context : Context){
         //Ouverture
         val dbwritable: SQLiteDatabase = this.database.writableDatabase
 
+        //Suppression des sous-tâches s'il y en a
+        if(promesse.subtasks != null) {
+            for(sub in promesse.subtasks!!) {
+                dbwritable.delete("Subtask","Subtask.Id_Subtask = ${sub.id}", null)
+            }
+        }
+        //Suppression de la promesse
         dbwritable.delete("Promise","Promise.Id_Promise = ${promesse.id}", null)
 
         //Fermeture
@@ -39,6 +43,7 @@ class PromiseDataBase (context : Context){
     fun addPromise(email : String, promise : Promise) {
         //Ouverture
         val dbwritable: SQLiteDatabase = this.database.writableDatabase
+
         //Ajout des valeurs
         val values = ContentValues()
         values.put("Email", email)
@@ -47,21 +52,28 @@ class PromiseDataBase (context : Context){
         values.put("State", promise.state.toString())
         values.put("Priority", if(promise.priority) 1 else 0)
         values.put("Professional", if(promise.professional) 1 else 0)
-
-
         values.put("Date_Creation", dateFormat.format(promise.dateCreation))
         values.put("Date_Todo", dateFormat.format(promise.dateTodo))
         values.put("Description", promise.description)
 
+        val id = dbwritable.insert("Promise", null, values)
 
-        dbwritable.insert("Promise", null, values)
+        //Ajout des sous-tâches
+        for(sub in promise.subtasks!!) {
+            println("oo???")
+            val subvalues = ContentValues()
+            subvalues.put("Id_Promise", id)
+            subvalues.put("Title", sub.title)
+            subvalues.put("Done", sub.done)
+            dbwritable.insert("Subtask", null, subvalues)
+        }
+
         //Fermeture
         dbwritable.close()
     }
 
     fun getAllPromises(email : String) : Set<Promise> {
         val dbreadable : SQLiteDatabase = this.database.readableDatabase
-        //Execution requête
 
         //Execution requête
         val col = arrayOf("Id_Promise", "Title", "Duration", "State", "Priority", "Description", "Professional", "Date_Creation", "Date_Todo")
@@ -85,7 +97,17 @@ class PromiseDataBase (context : Context){
                 val dateCreation = dateFormat.parse(curs.getString(curs.getColumnIndexOrThrow("Date_Creation")))
                 val dateTodo = dateFormat.parse(curs.getString(curs.getColumnIndexOrThrow("Date_Todo")))
 
-                promiseList.add(Promise(id, title,duration,state,priority,description,professional,dateCreation,dateTodo, null))
+                val subtasks = mutableListOf<Subtask>()
+                val curs2 = dbreadable.query("Subtask", arrayOf("Id_Subtask", "Title", "Done"), "Id_Promise = $id", null, null, null, null)
+                try {
+                    println("aa??")
+                    while(curs2.moveToNext()) {
+                        subtasks.add(Subtask(curs2.getInt(curs2.getColumnIndexOrThrow("Id_Subtask")), curs2.getString(curs2.getColumnIndexOrThrow("Title")), curs2.getInt(curs2.getColumnIndexOrThrow("Done"))>0))
+                    }
+                } finally {
+                    curs2.close()
+                }
+                promiseList.add(Promise(id, title,duration,state,priority,description,professional,dateCreation,dateTodo, if (subtasks.isNotEmpty()) subtasks else null))
 
             }
         } finally {
